8ca81f0bf78123092d529ed718ed8a9f98b6feb6
Bruno Martins Moreira <bruno_moreira.19@outlook.com>
2022-09-28 14:16:56 -0300

    refactor: eslint & prettier config

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
-import express from 'express'
-import { AppDataSource } from './database/data-source'
+import express from 'express';
+import { AppDataSource } from './database/data-source';
 
 AppDataSource.initialize().then(() => {
-	const app = express()
+  const app = express();
 
-	app.use(express.json())
+  app.use(express.json());
 
-	return app.listen(process.env.PORT, ()=>{
-    console.log(`Server is runing on port ${process.env.PORT}`)
-  })
-})
+  return app.listen(process.env.PORT, () => {
+    console.log(`Server is runing on port ${process.env.PORT}`);
+  });
+});
