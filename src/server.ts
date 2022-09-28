098505fe63e24616ffa8110b8f3639ccb45f13fd
Bruno Martins Moreira <bruno_moreira.19@outlook.com>
2022-09-28 13:29:17 -0300

    chore: config typeorm connection

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
-import express from "express";
+import express from 'express'
+import { AppDataSource } from './database/data-source'
 
-const app = express();
+AppDataSource.initialize().then(() => {
+	const app = express()
 
-app.listen(3333, () => {
-  console.log("server is running!");
-});
+	app.use(express.json())
+
+	return app.listen(process.env.PORT, ()=>{
+    console.log(`Server is runing on port ${process.env.PORT}`)
+  })
+})
